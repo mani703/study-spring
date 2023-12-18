4c5bd3034f9aae89f15f18134f95daa12ec453cc
mani703 <mani703@naver.com>
2023-12-18 19:24:57 +0900

    practice response case

--- src/main/java/com/example/hello/controller/ApiController.java
+++ src/main/java/com/example/hello/controller/ApiController.java
@@ -1,15 +1,30 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    @GetMapping("/hello")   // http://localhost:8080/api/hello
-    public String hello(){
-        return "hello spring boot!";
+    // TEXT
+    @GetMapping("/text")
+    public String text(@RequestParam String account) {
+        return account;
+    }
+
+    // JSON
+    // req -> object mapper -> object -> method -> object -> object mapper -> json -> response
+    @PostMapping("/json")
+    public User json(@RequestBody User user){
+        return user;    // 200 OK
+    }
+
+    // ResponseEntity
+    @PutMapping("/put")
+    public ResponseEntity<User> put(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 }
